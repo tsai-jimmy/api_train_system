b307d0be4da8602468023e8e0c42aebe465d2d8a
JimmyTsai <qaz3182@gmail.com>
2020-01-14 20:40:00 +0800

    add welcom index and devise user

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   namespace :api, :defaults => { :format => :json } do
     namespace :v1 do
       resources :trains, :only => [:index, :show]
       resources :reservations, param: :booking_code, :only => [:show, :create, :update, :destroy]
     end
   end
+  root "welcome#index"
 end
