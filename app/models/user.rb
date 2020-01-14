d7eebf3769149e9e87c18af4ba5759fd17869696
JimmyTsai <qaz3182@gmail.com>
2020-01-14 21:26:15 +0800

    add user authentication_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   has_many :reservations
+  before_create :generate_authentication_token
+
+  def generate_authentication_token
+    self.authentication_token = Devise.friendly_token
+  end
 end
