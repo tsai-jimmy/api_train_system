504501896657b1224963bf30e92270d00425fe70
JimmyTsai <qaz3182@gmail.com>
2020-01-14 21:58:38 +0800

    user can login logout signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :trains, :only => [:index, :show]
       resources :reservations, param: :booking_code, :only => [:index, :show, :create, :update, :destroy]
+
+      post "/signup" => "auth#signup"
+      post "/login" => "auth#login"
+      post "/logout" => "auth#logout"
     end
   end
   root "welcome#index"
