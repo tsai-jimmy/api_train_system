afd0a7725c9b5f83afda131e1819854c28e9b303
JimmyTsai <qaz3182@gmail.com>
2020-01-14 20:22:54 +0800

    refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
   namespace :api, :defaults => { :format => :json } do
     namespace :v1 do
-      get "/trains"  => "trains#index", :as => :trains
-      get "/trains/:train_number" => "trains#show", :as => :train
-
-      get "/reservations/:booking_code" => "reservations#show", :as => :reservation
-      post "/reservations" => "reservations#create", :as => :create_reservations
-      patch "/reservations/:booking_code" => "reservations#update", :as => :update_reservation
-      delete "/reservations/:booking_code" => "reservations#destroy", :as => :cancel_reservation
+      resources :trains, :only => [:index, :show]
+      resources :reservations, param: :booking_code, :only => [:show, :create, :update, :destroy]
     end
   end
 end
