10ec0a68c3b1336fc8537be0c53ab1fe8d15c29f
JimmyTsai <qaz3182@gmail.com>
2020-01-14 21:43:18 +0800

    add authenticate_user! for reservation index

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,4 +1,22 @@
 class Api::V1::ReservationsController < ApiController
+  before_action :authenticate_user!, :only => [:index] # 这会检查 index +这个操作一定要登入
+
+  def index
+    @reservations = current_user.reservations
+
+    render :json => {
+      :data => @reservations.map { |reservation|
+        {
+          :booking_code => reservation.booking_code,
+          :train_number => reservation.train.number,
+          :seat_number => reservation.seat_number,
+          :customer_name => reservation.customer_name,
+          :customer_phone => reservation.customer_phone
+        }
+      }
+    }
+  end
+
   def show
     @reservation = Reservation.find_by_booking_code!( params[:booking_code] )
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api, :defaults => { :format => :json } do
     namespace :v1 do
       resources :trains, :only => [:index, :show]
-      resources :reservations, param: :booking_code, :only => [:show, :create, :update, :destroy]
+      resources :reservations, param: :booking_code, :only => [:index, :show, :create, :update, :destroy]
     end
   end
   root "welcome#index"
